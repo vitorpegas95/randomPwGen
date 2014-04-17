3c95567ac720b9281fa7b0c1b440cbec7283518e
Vitor Pegas <vitorpegas95@gmail.com>
2014-04-17 22:48:44 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>RandomIZER</title>
+		<!-- Include the Style sheet -->
+		<link rel="stylesheet" type="text/css" href="style.css">
+		
+		<!-- Include the Random Script -->
+		<script type="text/javascript" src="random-min.js"></script>
+	</head>
+
+	<body>
+		<h1 class="Three-Dee">RandomIZER</h1>
+		
+		<div style="width: 480px; margin-top: 40px; margin-left: auto; margin-right: auto;">
+			<div class="button" onclick="GenRandom()">Generate</div>
+			<div id="random"></div>
+		</div>
+		
+		<p style="position: fixed; bottom: 0; right: 0;">Created by Vitor P&ecirc;gas 2013</p>
+	</body>	
+</html>
