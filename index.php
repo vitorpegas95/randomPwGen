32af21dab5d42ec69043a36817ac477a26596609
Vitor Pêgas <vitorpegas95@gmail.com>
2015-07-31 15:27:51 +0100

    Updated Index v2
    Version 2.

--- index.php
+++ index.php
@@ -1,22 +1,17 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>RandomIZER</title>
-		<!-- Include the Style sheet -->
-		<link rel="stylesheet" type="text/css" href="style.css">
-		
-		<!-- Include the Random Script -->
-		<script type="text/javascript" src="random-min.js"></script>
-	</head>
-
-	<body>
-		<h1 class="Three-Dee">RandomIZER</h1>
-		
-		<div style="width: 480px; margin-top: 40px; margin-left: auto; margin-right: auto;">
-			<div class="button" onclick="GenRandom()">Generate</div>
-			<div id="random"></div>
-		</div>
-		
-		<p style="position: fixed; bottom: 0; right: 0;">Created by Vitor P&ecirc;gas 2013</p>
-	</body>	
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Password Generator</title>
+        <script src="js/random.js"></script>
+    </head>
+    
+    <body>
+        <h1>Random Password Generator</h1>
+        <h3>15 chars: <a href="#" style="display:inline-block;" id="return15" onclick="this.innerHTML=GenPassword(15)">Click Me</a></h3>
+        <h3>10 chars: <a href="#" style="display:inline-block;" id="return15" onclick="this.innerHTML=GenPassword(10)">Click Me</a></h3>
+        <h3>08 chars: <a href="#" style="display:inline-block;" id="return15" onclick="this.innerHTML=GenPassword(8)">Click Me</a></h3>
+        
+        
+    </body>
 </html>
